8cacae97225da4094a5a3efc084bdd6adce9e9fd
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-02-22 18:45:55 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,13 @@ export default defineConfig((env) => {
       host: '0.0.0.0',
       port: 1002,
       open: false,
-      proxy: createViteProxy(isOpenProxy, viteEnv),
+      proxy: {
+        '/api': {
+        target: viteEnv.VITE_APP_API_BASE_URL,
+        changeOrigin: true, // 允许跨域
+        rewrite: path => path.replace('/api/', '/'),
+      },
+     },
     },
     build: {
       reportCompressedSize: false,
