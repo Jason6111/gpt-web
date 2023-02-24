99144be8a89811d158ba1974b3081d854e1cc77b
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-02-25 03:55:23 +0800

    2.7.3

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-center text-neutral-500">
-    <span>❤️ Star on</span>
+  <div class="text-neutral-400">
+    <span>Star on</span>
     <a href="https://github.com/Jason6111/gpt-web" target="_blank" class="text-blue-500">
       GitHub
     </a>
